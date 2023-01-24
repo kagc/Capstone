0489095572153996755123d42e394bc379034fe9
kirin <kirinagcaoili@gmail.com>
2023-01-23 19:59:30 -0800

    import models to init

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -1,3 +1,7 @@
 from .db import db
 from .user import User
+from .project import Project
+from .project_step import ProjectStep
+from .comment import Comment
+from .question import Question
 from .db import environment, SCHEMA

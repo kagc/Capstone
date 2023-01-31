138ca9b58a83efe082187887d9d4af32d7f930e5
kirin <kirinagcaoili@gmail.com>
2023-01-30 16:26:29 -0800

    date for project publish and comment posting working

--- app/seeds/comments.py
+++ app/seeds/comments.py
@@ -1,25 +1,30 @@
+from datetime import datetime
 from app.models import db, Comment, environment, SCHEMA
 
 def seed_comments():
     comment1 = Comment(
         projectId = 1,
         userId = 1,
-        comment = "Wow, super cool thanks."
+        comment = "Wow, super cool thanks.",
+        created_at = datetime(2023, 1, 28, 12, 00)
     )
     comment2 = Comment(
         projectId = 1,
         userId = 2,
-        comment = "I don't understand."
+        comment = "I don't understand.",
+        created_at = datetime(2023, 1, 29, 12, 00)
     )
     comment3 = Comment(
         projectId = 1,
         userId = 3,
-        comment = "Directions unclear. Cat bit me. :("
+        comment = "Directions unclear. Cat bit me. :(",
+        created_at = datetime(2023, 1, 29, 12, 30)
     )
     comment4 = Comment(
         projectId = 1,
         userId = 1,
-        comment = "Oh dang."
+        comment = "Oh dang.",
+        created_at = datetime(2023, 1, 29, 12, 45)
     )
     # comment1 = Comment(
     #     projectId = 1,
